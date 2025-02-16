775704825f243a076b1ba627a2cf60f9483af8de
BugHunter-X <askzenai@gmail.com>
2025-02-16 02:06:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This error occurs when you try to import a module that Expo doesn't support or that's not properly installed.  It often manifests when using certain third-party libraries or features that rely on native modules or require specific configurations within Expo's managed workflow.
